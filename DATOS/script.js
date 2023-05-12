bd0ebe382d595713c49dd5671d98f85d01d5795e
Veronica Marquez <72827226+VeronicaMrqz@users.noreply.github.com>
2023-05-11 21:32:40 -0600

    Add files via upload

---
+++ DATOS/script.js
@@ -0,0 +1,20 @@
+function Generador(){
+var cantidadNumeros = 8;
+var myArray = []
+while(myArray.length < cantidadNumeros ){
+  var numeroAleatorio = Math.ceil(Math.random()*cantidadNumeros);
+  var existe = false;
+  for(var i=0;i<myArray.length;i++){
+	if(myArray [i] == numeroAleatorio){
+        existe = true;
+        break;
+    }
+  }
+  if(!existe){
+    myArray[myArray.length] = numeroAleatorio;
+  }
+
+}
+alert("tu clave es: "+ myArray);
+
+}
